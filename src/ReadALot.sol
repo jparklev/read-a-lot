aae98f27bff84247f32a6844d4d7c0ae88ce39aa
Joshua Levine <joshuapark95@gmail.com>
2018-10-06 23:57:13 -0700

    import read-a-lot contract

--- src/ReadALot.sol
+++ src/ReadALot.sol
@@ -1,4 +1,29 @@
-pragma solidity ^0.4.24;
+pragma solidity 0.4.24;
 
 contract ReadALot {
+    function aggregate(bytes data) public view returns (bytes) {
+        uint256 malloc;
+        assembly { malloc := mul(mload(add(data, 0x20)), 0x20) }
+        bytes memory tempBytes = new bytes(malloc);
+        assembly {
+            let ptr := mload(0x40) 
+            let cur := 0x40
+            let inc := 1
+            let len := mload(data)
+
+            for { } lt(cur, len) { } {
+                let _target     := mload(add(data, cur))
+                let _retLen     := mul(mload(add(data, add(cur, 0x20))), 0x20)
+                let _dataLength := mload(add(data, add(cur, 0x60)))
+                let _data       := add(data, add(cur, 0x80))
+                if eq(call(gas, _target, 0, _data, _dataLength, ptr, _retLen), 0) 
+                    { revert(0, 0) }
+                let _retVal := mload(ptr)
+                mstore(add(tempBytes, mul(inc, 0x20)), _retVal)
+                inc := add(inc, mload(add(data, add(cur, 0x20))))
+                cur := add(cur, add(0x80, mul(div(add(_dataLength, 0x1f), 0x20), 0x20)))
+            }
+        }
+        return tempBytes;
+    }
 }
--- src/ReadALot.t.sol
+++ src/ReadALot.t.sol
@@ -4,18 +4,39 @@ import "ds-test/test.sol";
 
 import "./ReadALot.sol";
 
-contract ReadALotTest is DSTest {
-    ReadALot lot;
+contract Store {
+    uint256 internal val;
+    function set(uint256 _val) public { val = _val; }
+    function get() public view returns (uint256, uint256) { return (val, val); }
+    function getAdd(uint256 _val) public view returns (uint256) { return val + _val; }
+}
+
+contract MultiCallTest is DSTest {
+    using BytesLib for bytes;
+
+    MultiCall multicall;
+    Store        storeA;
+    Store        storeB;
 
     function setUp() public {
-        lot = new ReadALot();
+        multicall = new MultiCall();
+        storeA    = new Store();
+        storeB    = new Store();
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
+    function test_store_basic_sanity() public {
+        assertEq(storeA.get(), 0);
+        storeA.set(100);
+        assertEq(storeA.get(), 100);
+        storeA.set(0);
+        assertEq(storeA.get(), 0);
     }
 
-    function test_basic_sanity() public {
-        assertTrue(true);
-    }
+    // event A(bytes t);
+    // function test_single_call_no_args() public {
+    //     storeA.set(123);
+    //     bytes memory a = abi.encode(storeA, abi.encodeWithSignature("balanceOf(address)", 0x40502eaa3fcfd60ef97162a8b2246995e42dbeac));
+    //     emit A(a);
+    //     assertTrue(false);
+    // }
 }
