0267a9540ac03037e364921f861d0f910fd106ea
Joshua Levine <joshuapark95@gmail.com>
2018-10-07 19:48:21 -0700

    add block number to return values

--- src/ReadALot.sol
+++ src/ReadALot.sol
@@ -1,14 +1,16 @@
-pragma solidity 0.4.24;
+pragma solidity ^0.4.24;
 
 contract ReadALot {
     function aggregate(bytes data) public view returns (bytes) {
         uint256 malloc;
-        assembly { malloc := mul(mload(add(data, 0x20)), 0x20) }
+        assembly { malloc := add(mul(mload(add(data, 0x20)), 0x20), 0x20) }
         bytes memory tempBytes = new bytes(malloc);
+        uint256 _block = block.number;
         assembly {
+            mstore(add(tempBytes, 0x20), _block)
             let ptr := mload(0x40) 
             let cur := 0x40
-            let inc := 1
+            let inc := 2
             let len := mload(data)
 
             for { } lt(cur, len) { } {
@@ -20,8 +22,8 @@ contract ReadALot {
                     { revert(0, 0) }
                 let _retVal := mload(ptr)
                 mstore(add(tempBytes, mul(inc, 0x20)), _retVal)
-                inc := add(inc, mload(add(data, add(cur, 0x20))))
-                cur := add(cur, add(0x80, mul(div(add(_dataLength, 0x1f), 0x20), 0x20)))
+                inc := add(inc, 1)
+                cur := add(cur, add(0x80, _dataLength))
             }
         }
         return tempBytes;
